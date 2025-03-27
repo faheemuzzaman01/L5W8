27d6dcc64b5e84296bc8cc1b598b8eb2816c66ed
faheemuzzaman01 <161707364+faheemuzzaman01@users.noreply.github.com>
2025-03-27 14:04:12 +0000

    test01

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ app.get("/", (req, res) => {
     res.send("Hello from NCG");
 });
 
-app.listen(3000,"Server is up and running on port 3000")
+app.listen(3000, () => console.log("Server is running on port 3000"));
