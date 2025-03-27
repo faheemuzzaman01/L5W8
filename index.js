72c390e7d14b58ec57e2bc67803c88e27cb82de1
faheemuzzaman01 <161707364+faheemuzzaman01@users.noreply.github.com>
2025-03-27 14:05:56 +0000

    listen function changed

--- index.js
+++ index.js
@@ -28,4 +28,5 @@ app.get("/", (req, res) => {
     res.send("Hello from NCG");
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+
+app.listen(3000,()=>{console.log("Server is up on port 3000")})
